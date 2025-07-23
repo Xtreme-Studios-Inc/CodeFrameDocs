b090fc56381db9853c3378bb4edfeb41948c2b21
xsandre-l <ahloubser12@gmail.com>
2025-07-23 18:33:06 +0200

    .

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://codeframe.run",
+  url: "https://xcodeframe.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
