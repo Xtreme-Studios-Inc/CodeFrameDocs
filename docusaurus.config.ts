809784d93841805bfed8e617f5f82f0d71aa5603
xsandre-l <ahloubser12@gmail.com>
2025-07-23 08:43:00 +0200

    .

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Xtreme-Studios-Inc", // Usually your GitHub org/user name.
-  projectName: "XCodeFrame", // Usually your repo name.
+  projectName: "CodeFrameDocs", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
